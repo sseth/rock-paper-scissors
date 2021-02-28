ca0114c37e8f2838f5625a30fa006520fbd3eb3f
Shubhankar Seth <shubhseth@gmail.com>
2021-02-28 15:40:42 +0530

    Refactor

--- app.js
+++ app.js
@@ -5,50 +5,50 @@ var userScore = 0, compScore = 0;
 const rock = document.querySelector('#rock > img');
 const paper = document.querySelector('#paper > img');
 const scissors = document.querySelector('#scissors > img');
+const message = document.getElementById('message');
+const uscoreElem = document.getElementById('u-score');
+const cscoreElem = document.getElementById('c-score');
+
+function getCompMove() {
+    let rand = Math.floor(Math.random() * 3);
+    return ['Rock', 'Paper', 'Scissors'][rand];
+}
+
+function win(c) {
+    userScore++;
+    uscoreElem.innerHTML = userScore;
+    message.innerHTML = c + ": You win";
+}
+
+function lose(c) {
+    compScore++;
+    cscoreElem.innerHTML = compScore;
+    message.innerHTML = c + ": You lose";
+}
+
+function draw(c) {
+    message.innerHTML = c + ": It's a draw";
+}
 
 function play() {
-    const message = document.getElementById('message');
-    let compMove = Math.floor(Math.random() * 3);
-    console.log(compMove);
-    // 0 = rock, 1 = paper, 2 = scissors
+    let compMove = getCompMove();
 
-    switch(this) {
-        case rock:
-            if (compMove == 0) { 
-                message.innerHTML = "Tie";
-            } else if (compMove == 1) { 
-                compScore++;
-                message.innerHTML = "You lose";
-            } else {
-                userScore++; 
-                message.innerHTML = "You win";
-            }
+    switch(this.title + compMove) {
+        case 'RockScissors':
+        case 'ScissorsPaper':
+        case 'PaperRock':
+            win(compMove);
             break;
-        case paper:
-            if (compMove == 0) {
-                userScore++; 
-                message.innerHTML = "You win";
-            } else if (compMove == 1) {
-                message.innerHTML = "Tie";
-            } else {
-                compScore++;
-                message.innerHTML = "You lose";
-            }
+        case 'RockRock':
+        case 'PaperPaper':
+        case 'ScissorsScissors':
+            draw(compMove);
             break;
-        case scissors:
-            if (compMove == 0) { 
-                compScore++; 
-                message.innerHTML = "You lose";
-            } else if (compMove == 1) { 
-                userScore++;
-                message.innerHTML = "You win";
-            } else {
-                message.innerHTML = "Tie";
-            }
+        case 'RockPaper':
+        case 'PaperScissors':
+        case 'ScissorsRock':
+            lose(compMove);
     }
-
-    document.getElementById('p-score').innerHTML = userScore;
-    document.getElementById('c-score').innerHTML = compScore;
 
 }
 
